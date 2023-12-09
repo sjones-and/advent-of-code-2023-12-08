263ec50f3d65b8f7b2229dc24cadf68e4d79cbc6
Stuart Jones <47328516+sjones-and@users.noreply.github.com>
2023-12-09 11:41:28 +0000

    Day eight solution

--- part-one.py
+++ part-one.py
@@ -3,12 +3,44 @@
 import os
 from time import perf_counter_ns
 
+class Node:
+    nodes = {}
+
+    def connect_all():
+        for node in Node.nodes.values():
+            node.connect()
+
+    def __init__(self, data):
+        self.name = data[0:3]
+        self.leftName = data[7:10]
+        self.rightName = data[12:15]
+        self.left = None
+        self.right = None
+        Node.nodes[self.name] = self
+
+    def connect(self):
+        self.left = Node.nodes[self.leftName]
+        self.right = Node.nodes[self.rightName]
+
+    def move(self, dir):
+        return self.left if dir == 'L' else self.right
+
 def answer(input_file):
     start = perf_counter_ns()
     with open(input_file, 'r') as input:
         data = input.read().split('\n')
 
-    answer = None
+    instructions = data[0]
+    _ = [Node(line) for line in data[2:]]
+    Node.connect_all()
+
+    answer = 0
+    instructions_length = len(instructions)
+    current_node = Node.nodes['AAA']
+    while current_node.name != 'ZZZ':
+        current_node = current_node.move(instructions[answer % instructions_length])
+        answer += 1
+
     end = perf_counter_ns()
 
     print(f'The answer is: {answer}')
--- part-two.py
+++ part-two.py
@@ -2,17 +2,73 @@
 
 import os
 from time import perf_counter_ns
+from math import lcm
+
+class Node:
+    nodes = {}
+
+    def connect_all():
+        for node in Node.nodes.values():
+            node.connect()
+
+    def reset_all():
+        for node in Node.nodes.values():
+            node.reset()
+
+    def __init__(self, data):
+        self.name = data[0:3]
+        self.leftName = data[7:10]
+        self.rightName = data[12:15]
+        self.left = None
+        self.right = None
+        self.positions = {}
+        self.period = None
+        Node.nodes[self.name] = self
+
+    def connect(self):
+        self.left = Node.nodes[self.leftName]
+        self.right = Node.nodes[self.rightName]
+
+    def move(self, position, length, dir):
+        output = self.left if dir == 'L' else self.right
+        if position % length in self.positions.keys():
+            self.period = position - self.positions[position % length]
+            return self
+        if self.name[-1] == 'Z':
+            self.positions[position % length] = position
+        return output
+    
+    def reset(self):
+        self.positions = {}
+        self.period = None
 
 def answer(input_file):
     start = perf_counter_ns()
-    with open(input_file, "r") as input:
+    with open(input_file, 'r') as input:
         data = input.read().split('\n')
 
-    answer = None
+    instructions = data[0]
+    _ = [Node(line) for line in data[2:]]
+    Node.connect_all()
+
+    instructions_length = len(instructions)
+    positions = []
+    for node in Node.nodes.values():
+        if node.name[-1] == 'A':
+            Node.reset_all()
+            ix = 0
+            current_node = node
+            while not current_node.period:
+                instruction = instructions[ix % instructions_length]
+                current_node = current_node.move(ix, instructions_length, instruction)
+                ix += 1
+            positions.append(current_node.period)
+
+    answer = lcm(*positions)
     end = perf_counter_ns()
 
     print(f'The answer is: {answer}')
     print(f'{((end-start)/1000000):.2f} milliseconds')
 
-input_file = os.path.join(os.path.dirname(__file__), "input")
+input_file = os.path.join(os.path.dirname(__file__), 'input')
 answer(input_file)
